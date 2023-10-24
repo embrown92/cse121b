a753e9c4703aac86c475a2f89e5b0fdaed63da2b
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2023-10-24 12:04:26 -0400

    Added an popular keyword search array

---
+++ Project/scripts/keywords.js
@@ -0,0 +1,39 @@
+const keyElement = document.getElementById("popular-keywords");
+
+
+let keywords = [
+    "Thanksgiving",
+    " Christmas",
+    " American Flag",
+    " Family",
+    " Memorial Day",
+    " Halloween",
+    " Money",
+    " Business",
+    " Veterans Day",
+    " Social Media",
+    " Thank you",
+    " Happy Birthday",
+    " Arrow",
+    " Technology",
+    " Labor Day",
+    " Beach",
+    " House",
+    " Data",
+    " Community",
+    " Heart",
+    " Mental Health",
+    " Background",
+    " Fireworks",
+    " Laptop",
+    " Sports",
+    " Happy New Year",
+    " Baby",
+    " Summer",
+    " Fall",
+    " Winter",
+    " Happy",
+    " Love"
+];
+
+keyElement.innerHTML = `${keywords}`;
